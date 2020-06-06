5837b1d32dfaae1121df634068b3b67ecde2943d
hudadamar21 <hudadamar21@gmail.com>
2020-06-06 17:50:43 +0700

    Updates

---
+++ precache-manifest.0e6ba14ba3550537c14236724e01c858.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85d339bf8cc49194eeb7d3ed5bf8e3d2",
+    "url": "/smovies/index.html"
+  },
+  {
+    "revision": "455cc2d5a9fde7502f6f",
+    "url": "/smovies/static/css/main.6ad71354.chunk.css"
+  },
+  {
+    "revision": "34f9424496d5a475441a",
+    "url": "/smovies/static/js/2.fa0744c0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/smovies/static/js/2.fa0744c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "455cc2d5a9fde7502f6f",
+    "url": "/smovies/static/js/main.357b4437.chunk.js"
+  },
+  {
+    "revision": "d4445b789c6f72b8e184",
+    "url": "/smovies/static/js/runtime-main.785ffbdc.js"
+  },
+  {
+    "revision": "602d77cc948e29730b3e9315f32336d8",
+    "url": "/smovies/static/media/sans.602d77cc.jpg"
+  }
+]);
